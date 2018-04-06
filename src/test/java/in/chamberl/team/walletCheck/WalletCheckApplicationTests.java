577d593271d129b131b1132064281dc984a1fae5
Crew Chamberlin <crew@chamberl.in>
2018-04-06 17:21:30 -0400

    initial commit from spring.io

---
+++ src/test/java/in/chamberl/team/walletCheck/WalletCheckApplicationTests.java
@@ -0,0 +1,16 @@
+package in.chamberl.team.walletCheck;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WalletCheckApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
