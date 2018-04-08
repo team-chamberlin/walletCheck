af313fdf13a2b500b97e4c1b2a55308b5d14e764
Crew Chamberlin <crew@chamberl.in>
2018-04-07 20:56:32 -0400

    I think I got it

---
+++ src/main/java/in/chamberl/team/walletcheck/crew.java/WalletCheckApplication.java
@@ -0,0 +1,12 @@
+package in.chamberl.team.walletCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletCheckApplication.class, args);
+	}
+}
