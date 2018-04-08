2e549f2eed72bc9b3485e345ed37a88939098c8b
Crew Chamberlin <crew@chamberl.in>
2018-04-08 16:49:10 -0400

    SOMETHING?!

--- src/main/java/in/chamberl/team/walletcheck/crew/Crew.java
+++ src/main/java/in/chamberl/team/walletcheck/crew/Crew.java
@@ -2,12 +2,18 @@ package in.chamberl.team.walletcheck.crew;
 
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellComponent;
+import java.io.UnsupportedEncodingException;
 
 @ShellComponent
 public class Crew {
 	@ShellMethod("Crew's command.")
-	public String crew(String input) {
-		int read(byte[] b)
-		return "input was:" + input;
+	public int crew(String input) throws UnsupportedEncodingException {
+		byte[] crewsstringsize = input.getBytes("US-ASCII");
+		return crewsstringsize.length;
+	}
+@ShellMethod("Crew's command.")
+	public byte[] kyle(String input) throws UnsupportedEncodingException {
+		byte[] crewsstringsize = input.getBytes("US-ASCII");
+		return crewsstringsize;
 	}
 }
